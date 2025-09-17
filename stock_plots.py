31303e4f7bd0c1821900707dcbd0223bb92e8877
Austin Rivera <amrivera5@mail.fhsu.edu>
2025-09-16 22:53:28 -0500

    Added imports and stock tickers

--- stock_plots.py
+++ stock_plots.py
@@ -1,3 +1,10 @@
 # INF601 - Advanced Programming in Python
 # Austin Rivera
 # Mini Project 1
+
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+tickers = ['AAPL', 'GOOGL', 'MSFT', 'TSLA', 'AMZN']
